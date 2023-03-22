61b83882b3bdf340ef685687439498d32e4708f8
itsAliTahir <115342949+itsAliTahir@users.noreply.github.com>
2023-03-22 21:34:34 +0500

    Guess The Number Game Done

---
+++ Guess the Number/GuessTheNumber.py
@@ -0,0 +1,24 @@
+import random
+num = random.randint(1,100)
+chances = 5
+guess = 0
+print(num)
+print("Guess The Random Generated Number From 1 to 100 (Chances:",chances,")\n")
+while chances > 0:
+    guess = input("Enter Your Number: ")
+    guess = int(guess)
+    if guess == num:
+        
+        print("You Have Guessed The Number Correctly!")
+        print("Your Score:", chances*20)
+        chances = 0
+    else:
+        chances-=1        
+        print("Incorrect Answer!")
+        if num > guess: print("Your Number ",guess, " is Smaller Than That Number" )
+        else: print("Your Number ",guess, " is Greater Than That Number" )
+        print("Chances Left:", chances, "\n")
+if guess != num:
+    print("You Lost!")
+    print("The Number Was: ", num)    
+    print("Your Score: 0")
